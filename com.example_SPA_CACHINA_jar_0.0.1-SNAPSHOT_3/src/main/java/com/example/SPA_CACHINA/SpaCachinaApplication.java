8089ec0da1d01d347f10adc7a5563669b0d7b050
Dave1901-sudo <u22244804@utp.edu.pe>
2026-07-19 16:19:03 -0500

    cambios fecha y hora bd

--- com.example_SPA_CACHINA_jar_0.0.1-SNAPSHOT_3/src/main/java/com/example/SPA_CACHINA/SpaCachinaApplication.java
+++ com.example_SPA_CACHINA_jar_0.0.1-SNAPSHOT_3/src/main/java/com/example/SPA_CACHINA/SpaCachinaApplication.java
@@ -1,5 +1,7 @@
 package com.example.SPA_CACHINA;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,6 +9,11 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.SPA_CACHINA")
 public class SpaCachinaApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpaCachinaApplication.class, args);
